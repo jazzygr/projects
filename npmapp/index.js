eb9d77c15312907f4acdb82ae7e5cef51c728bd9
Jeffrey  Graham <jeffgraham685@gmail.com>
2019-12-07 12:57:31 +0000

    security update

--- npmapp/index.js
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash');
-
-const numbers = [33, 43, 24, 22, 5, 2, 3];
-
-_.each(numbers, function(number, i){
-    console.log(number);
-});
--- webpacktut/src/bro.js
+++
@@ -1,5 +0,0 @@
-const bro = (greeting) => {
-    return `${greeting}, dawg`
-}
-
-export { bro }
